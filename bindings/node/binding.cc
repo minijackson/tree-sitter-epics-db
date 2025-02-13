90b4cba9c703516d0dce540a77a371ca88e36b7f
Minijackson <minijackson@riseup.net>
2025-02-13 11:23:46 +0100

    chore: update Node bindings

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -10,7 +10,6 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {
 };
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports["name"] = Napi::String::New(env, "epics_db");
     auto language = Napi::External<TSLanguage>::New(env, tree_sitter_epics_db());
     language.TypeTag(&LANGUAGE_TYPE_TAG);
     exports["language"] = language;
