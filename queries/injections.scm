b99f224f3d67104876c6638d1cf8cd2949e6e33f
Minijackson <minijackson@riseup.net>
2025-09-09 10:33:20 +0200

    feat: add C injection on dbd "extra" fields

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,3 +2,10 @@
 ((macro_expansion) @injection.content (#set! injection.language "epics_msi_template"))
 ((ccode) @injection.content (#set! injection.language "c"))
 ((json_value) @injection.content (#set! injection.language "json5"))
+
+(field_descriptor
+  (field_item) @_extra
+  value: (string
+		   (string_text_fragment) @injection.content
+		   (#set! injection.language "c"))
+  (#eq? @_extra "extra"))
