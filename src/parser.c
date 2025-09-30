fc5d356de4310866f8361c8c249afa1a74fc187f
Minijackson <minijackson@riseup.net>
2025-09-30 12:47:30 +0200

    chore: update version to 0.2.0

--- src/parser.c
+++ src/parser.c
@@ -6376,8 +6376,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_epics_db(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 7,
+      .minor_version = 2,
+      .patch_version = 0,
     },
   };
   return &language;
