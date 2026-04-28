cee7621ee42bcc305315649cadd276b06f4c7af6
Minijackson <minijackson@riseup.net>
2026-04-28 09:18:18 +0200

    fix(flake): don't use removed nodePackages

--- flake.nix
+++ flake.nix
@@ -1,18 +1,22 @@
 {
-  description = "A very basic flake";
+  description = "Development environment for tree-sitter";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }: {
-
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [ tree-sitter nodejs emscripten nodePackages.prettier ];
-      };
-
-    devShell.x86_64-linux = self.devShells.x86_64-linux.default;
-
-  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            tree-sitter
+            nodejs
+            emscripten
+            prettier
+          ];
+        };
+    };
 }
